6ff78c2ac58de262b4cff724803f61301c36f116
pandurang <pandurang.plw@gmail.com>
2013-08-26 15:42:34 +0530

    feature create fix

--- lib/feature_flags.rb
+++ lib/feature_flags.rb
@@ -1,6 +1,5 @@
 
 require "feature_flags/version"
-require "feature_flags/feature"
 require "feature_flags/configuration"
 require "feature_flags/engine"
 require "active_support/dependencies"
@@ -8,21 +7,21 @@ require "active_support/dependencies"
 module FeatureFlags
   # Your code goes here...\
   mattr_accessor :app_root
-  
-  def self.enabled?(feature)
-    feature = Feature.where(:name => feature).first
+
+  def self.enabled?(feature_name)
+    feature = Feature.where(:name => feature_name).first
 
     if feature.present? 
       feature.status? ? true : false
     elsif
-      Feature.create!(:name => feature, :status => true)
+      Feature.create!(:name => feature_name, :status => true)
       return true
     end
     
   end
 
-  def self.set_disabled?(feature)
-    feature = Feature.where(:name => feature).first
+  def self.set_disabled?(feature_name)
+    feature = Feature.where(:name => feature_name).first
     (feature.present? && feature.update_attributes(:status => false)) ? true : false
   end
 
