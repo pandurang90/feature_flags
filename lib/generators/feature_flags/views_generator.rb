5ef54986b5270f32b295992567553e0ddd6e2a7a
pandurang <pandurang.plw@gmail.com>
2013-08-28 13:01:54 +0530

    views generator fix

--- lib/generators/feature_flags/views_generator.rb
+++ lib/generators/feature_flags/views_generator.rb
@@ -4,15 +4,11 @@ module FeatureFlags
 
     class ViewsGenerator < Rails::Generators::Base
 
-      source_root File.expand_path("../../../../app/views/feature_flags", __FILE__)
+      source_root File.expand_path("../../../../app/views", __FILE__)
       desc "copies view files for feature_flags"
 
-      def copy_views_file
-        template "index.html.erb", "app/views/feature_flags/index.html.erb"
-        template "edit.html.erb", "app/views/feature_flags/edit.html.erb"
-        template "update.html.erb", "app/views/feature_flags/update.html.erb"
-        template "new.html.erb", "app/views/feature_flags/new.html.erb"
-        #say("added initializer file", :green)
+      def copy_views
+        directory 'feature_flags', 'app/views/feature_flags'
       end
 
     end
