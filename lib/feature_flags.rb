cf50fb575fc917c78bbb1e9a7db3a60ddc046277
pandurang <pandurang.plw@gmail.com>
2013-08-30 12:49:51 +0530

    module changes

--- lib/feature_flags.rb
+++ lib/feature_flags.rb
@@ -1,40 +1,10 @@
 
 require "feature_flags/version"
 require "feature_flags/configuration"
+require "feature_flags/manage_features"
 require "feature_flags/engine"
 require "active_support/dependencies"
 
 module FeatureFlags
   # Your code goes here...\
-
-  def self.enabled?(feature_name)
-    feature = Feature.where(:name => feature_name).first
-
-    if feature.present? 
-      feature.status? ? true : false
-    elsif
-      #Feature.create!(:name => feature_name, :status => true)
-      #return true
-
-      raise "#{feature_name} feature not found."
-    end
-    
-  end
-
-  def self.create_and_enable
-  end
-
-  def self.set_disabled(feature_name)
-    feature = Feature.where(:name => feature_name).first
-    (feature.present? && feature.update_attributes(:status => false)) ? true : false
-  end
-
-  def self.disable_all
-    Feature.update_all(:status => false)
-  end
-
-  def self.enable_all
-    Feature.update_all(:status => true)
-  end
-
 end
---
+++ lib/feature_flags/manage_features.rb
@@ -0,0 +1,56 @@
+module FeatureFlags
+  def self.enabled?(feature_name)
+    feature = Feature.where(:name => feature_name).first
+
+    if feature.present? 
+      feature.status? ? true : false
+    elsif
+      #Feature.create!(:name => feature_name, :status => true)
+      #return true
+      raise "#{feature_name} feature not found."
+    end
+    
+  end
+
+  def self.create_and_enable(feature_name)
+    feature = Feature.new(:name => feature_name, :status => true)
+    feature.save ? true : false
+  end
+
+  def self.enable(:feature_name)
+    feature = Feature.where(:name => feature_name).first
+    if feature.present? 
+      
+      if feature.update_attributes(:status => true)) 
+        return true
+      else
+        false
+      end
+
+      raise "#{feature_name} feature not found."
+    end
+  end
+
+  def self.set_disabled(feature_name)
+    feature = Feature.where(:name => feature_name).first
+    if feature.present? 
+
+      if feature.update_attributes(:status => false)) 
+        return true
+      else
+        false
+      end
+
+      raise "#{feature_name} feature not found."
+    end
+  end
+
+  def self.disable_all
+    Feature.update_all(:status => false)
+  end
+
+  def self.enable_all
+    Feature.update_all(:status => true)
+  end
+
+end
