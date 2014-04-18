455c96ab290f4f971ba03a2bc987736fa9dabfc8
Pandurang Waghulde <pandurang.plw@gmail.com>
2014-04-18 13:24:31 +0530

    test

--- lib/generators/feature_flags/views_generator.rb
+++ lib/generators/feature_flags/views_generator.rb
@@ -1,4 +1,3 @@
-
 module FeatureFlags
   module Generators
 
@@ -10,7 +9,8 @@ module FeatureFlags
       def copy_views
         directory 'feature_flags', 'app/views/feature_flags'
       end
-
+      
+      desc "copies view files for feature_flags"
     end
   end
 
